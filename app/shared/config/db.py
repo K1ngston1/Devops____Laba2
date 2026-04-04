bc8f68dec12b599daf9753d9f6b20fd42b06bf72
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2026-04-04 21:30:05 +0300

    Fix postgres engine creation

--- app/shared/config/db.py
+++ app/shared/config/db.py
@@ -2,21 +2,14 @@ from enum import Enum
 from sqlalchemy.engine import Engine
 from sqlalchemy import create_engine
 
-from app.shared.exceptions import DataSourceNotFoundException
-
-from .env import env_settings
+from .env import get_env_settings
 
 
 class DataSource(Enum):
     POSTGRES = "postgres"
 
 
-postgres_engine = create_engine(env_settings.postgres_url)
-
-
 def get_db_engine(data_source: DataSource) -> Engine:
     match data_source:
         case DataSource.POSTGRES:
-            return postgres_engine
-        case _:
-            raise DataSourceNotFoundException(data_source)
+            return create_engine(get_env_settings().postgres_url)
